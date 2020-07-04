9dfc461266b261ff8c36d31add512df69d07f4cb
vjeuel <vjeuel@gmail.com>
2020-07-04 00:26:15 -0500

    JAWSDB added

--- README.md
+++ README.md
@@ -9,10 +9,6 @@ You can access the app [here](https://morning-savannah-57705.herokuapp.com/)
 
 The Burger Joint is an app that let you create, devour and trash burgers, the technology used for this app include Node.js, MySQL database, Node Packages like express, express-handlebars and mysql and all deployed on Heroku, including the database where there is no need for you to install any package dependencies nor have to run the database in your computer.
 
-## Contents<br>
-**[License](#License)**</br>
-**[Contact](#Contact)**<br>
-
 ## **License**<br>
 mit
 
--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,26 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//    host: "localhost",
-//    port: 3306,
-//    user: "root",
-//    password: "",
-//    database: "burgers_db"
-// });
-
-const connectionConfig = process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
-   host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-   port: 3306,
-   user: "l37uf6rbn95yydgh",
-   password: "za70xg6svgs6drn2",
-   database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+   let connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+   const connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "",
+      database: "burgers_db"
+   });
 };
+   
+// const connectionConfig = process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
+//    host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//    port: 3306,
+//    user: "l37uf6rbn95yydgh",
+//    password: "za70xg6svgs6drn2",
+//    database: "burgers_db"
+// };
 
-const connection = mysql.createConnection(connectionConfig);
+// const connection = mysql.createConnection(connectionConfig);
 
 connection.connect(err => {
    if (err) {
